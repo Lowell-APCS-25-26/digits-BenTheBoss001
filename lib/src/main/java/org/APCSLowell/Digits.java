efbd958568cb9943a5e545fa050718c54d78b1ff
BenTheBoss001 <ben1122@yahoo.com>
2026-04-09 16:31:53 +0000

    add

--- lib/src/main/java/org/APCSLowell/Digits.java
+++ lib/src/main/java/org/APCSLowell/Digits.java
@@ -7,20 +7,33 @@ public class Digits {
      *  The digits appear in the list in the same order in which they appear in the original number
      */
     public ArrayList<Integer> digits;
+    
 
     /** Constructs a `Digits` object that represents `num`.
      *  *Precondition*: `num >= 0`
      */
     public Digits(int num) {
-        /* To be implemented in part (a) */
-        throw new UnsupportedOperationException();
+        digits = new ArrayList<Integer>();
+        String str = Integer.toString(num);
+        for(int i = 0; i < str.length(); i++){
+            digits.add(Integer.parseInt(str.substring(i,i+1)));
+        }
+        //throw new UnsupportedOperationException();
     }
 
     /** Returns `true` if the digits in this `Digits` object are in strictly increasing order;
      *      `false` otherwise.
      */
     public boolean isStrictlyIncreasing() {
-        /* To be implemented in part (b) */
-        throw new UnsupportedOperationException();
+        if (digits.size() <= 1){
+            return true;
+        }
+        for (int i = 1; i < digits.size(); i++){
+            if (digits.get(i) < digits.get(i-1)){
+                return false;
+            }
+        }
+        return true;
+        //throw new UnsupportedOperationException();
     }
 }
